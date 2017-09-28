d85f4c3c72b0f75b344f43919fc219674653c926
Kartikye <kartikye.mittal@gmail.com>
2017-09-28 00:36:47 +0000

    fixing add class

--- webviews/webviews.py
+++ webviews/webviews.py
@@ -1,7 +1,16 @@
 from flask import Blueprint, render_template, abort, request
 from jinja2 import TemplateNotFound
+from db import db_config
+import json
+
+db = db_config.create_db_connection()
 
 webview = Blueprint('webview', __name__, template_folder='templates')
+
+@webview.route('/subjects/<msg_id>', methods=['GET'])
+def subjects(msg_id):
+    s = [y[0] for y in db.execute('select subject from Subjects join Users on Subjects.userid = Users.user_id where Users.msg_id = ?', (msg_id,)).fetchall()]    
+    return json.dumps(s)
 
 @webview.route('/add_class', methods=["GET", "POST"])
 def show():
